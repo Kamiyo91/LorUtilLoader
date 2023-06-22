c2cdd012794b1d76b0aadfdbc08e7e8cb7774ee3
Kamiyo <kamiyo.k91@gmail.com>
2023-06-22 20:26:25 +0200

    DiceEffect Fix

--- Util/DiceEffectUtil.cs
+++ Util/DiceEffectUtil.cs
@@ -1,31 +1,30 @@
 ﻿using System.IO;
+using Battle.DiceAttackEffect;
 using UnityEngine;
 
 namespace UtilLoader21341.Util
 {
     public static class DiceEffectUtil
     {
-        public static void InitializeEffect<T>(float? _duration,
-            float positionX, float positionY, bool overSelf, BattleUnitView self, BattleUnitView target,
-            float destroyTime,
-            string path,
-            GameObject gameObject, ref BattleUnitModel _self, ref Transform selfTransform,
-            ref Transform targetTransform, ref float duration, ref SpriteRenderer spr, Transform transform
-        )
+        public static void InitializeEffect<T>(string path, float positionX, float positionY, bool overSelf, T ef,
+            BattleUnitView self, BattleUnitView target) where T : DiceAttackEffect
         {
-            //base.Initialize(self, target, destroyTime);
-            _self = self.model;
-            selfTransform = self.atkEffectRoot;
-            targetTransform = overSelf ? self.atkEffectRoot : target.atkEffectRoot;
-            transform.parent = overSelf ? self.charAppearance.transform : target.transform;
-            duration = _duration ?? destroyTime;
+            ef._self = self.model;
+            ef._selfTransform = self.atkEffectRoot;
+            ef._targetTransform = overSelf ? self.atkEffectRoot : target.atkEffectRoot;
+            ef.transform.parent = overSelf ? self.charAppearance.transform : target.transform;
             var texture2D = new Texture2D(1, 1);
             texture2D.LoadImage(File.ReadAllBytes(path + "/CustomEffect/" +
                                                   typeof(T).Name.Replace("DiceAttackEffect_", "") + ".png"));
-            spr.sprite = Sprite.Create(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height),
+            ef.spr.sprite = Sprite.Create(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height),
                 new Vector2(positionX, positionY));
-            gameObject.layer = LayerMask.NameToLayer("Effect");
-            //ResetLocalTransform(transform);
+            ef.gameObject.layer = LayerMask.NameToLayer("Effect");
+            ef.ResetLocalTransform(ef.transform);
+        }
+
+        public static float CalculateScale(bool dynamicScale, float scaleFactor, float scale)
+        {
+            return dynamicScale ? scaleFactor * scale : scale;
         }
     }
 }
